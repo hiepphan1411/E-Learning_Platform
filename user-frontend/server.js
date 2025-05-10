d15844eb79eb21d0ad6a76e0bf9d6f77a7f91781
pphiep <phanphuochiep2004@gmail.com>
2025-05-10 15:14:56 +0700

    Update server.js

--- user-frontend/server.js
+++ user-frontend/server.js
@@ -4,7 +4,8 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" })); 
+app.use(express.urlencoded({ limit: "50mb", extended: true })); 
 
 // 1) Kết nối đúng DB coursedb
 mongoose
